81ed5d5c2684ac4542345af9ffbaf2c42cc40989
joshyx <joshyx@40bb2de6-3f2a-0410-b0ae-47b76eba00f0>
2007-02-26 01:57:49 +0000

    initial import

---
+++ trunk/std_desklets/Clock/src/clockdesklet/Desklet.java
@@ -0,0 +1,78 @@
+/*
+ * Desklet.java
+ *
+ * Created on August 4, 2006, 8:48 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package clockdesklet;
+
+import ab5k.desklet.AbstractDesklet;
+import ab5k.desklet.DeskletContext;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author cooper
+ */
+public class Desklet extends AbstractDesklet{
+    
+    private ClockDisplay display;
+    private boolean running = false;
+    final SimpleDateFormat format = new SimpleDateFormat("hh:mm");
+    
+    /** Creates a new instance of Desklet */
+    public Desklet() {
+        super();
+    }
+    
+    public void destroy() throws Exception {
+    }
+    
+    public void start() throws Exception {
+        new Thread(new Runnable() {
+            public void run() {
+                while(running) {
+                    try {
+                        Thread.sleep(1000);
+                        
+                        SwingUtilities.invokeLater(new Runnable() {
+                            public void run() {
+                                display.currentTime.setText(format.format(new Date()));
+                            }
+                        });
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                System.out.println("notifying of stoppage");
+                context.notifyStopped();
+            }
+        }).start();
+    }
+    
+    
+    public void stop() throws Exception {
+        this.running = false;
+    }
+    
+    public void init(DeskletContext context) throws Exception {
+        this.context = context;
+        running = true;
+        display = new ClockDisplay();
+        display.setOpaque( false );
+        context.getContainer().setContent(this.display);
+        display.currentTime.setText(format.format(new Date()));
+        context.getContainer().setShaped(true);
+        context.getContainer().setBackgroundDraggable(true);
+        context.getContainer().setResizable(false);
+        context.getContainer().setVisible(true);
+    }
+    
+    
+    
+}
