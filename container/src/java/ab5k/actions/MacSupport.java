0de6fe36554eb40878690197c183541e3c02abaa
joshyx <joshyx@40bb2de6-3f2a-0410-b0ae-47b76eba00f0>
2007-02-27 06:03:42 +0000

    small updates

--- container/src/java/ab5k/actions/MacSupport.java
+++ container/src/java/ab5k/actions/MacSupport.java
@@ -27,46 +27,52 @@ import org.joshy.util.u;
  */
 public class MacSupport {
     public static void setupMacSupport(final Main main) {
-        u.p("installing mac support");
-        Application.getApplication().addApplicationListener(new ApplicationListener() {
-            public void handleAbout(ApplicationEvent applicationEvent) {
-                u.p("Apple API: about called:" + applicationEvent);
-            }
-            public void handleOpenApplication(ApplicationEvent applicationEvent) {
-                u.p("Apple API: open app called:" + applicationEvent);
-            }
-            public void handleOpenFile(ApplicationEvent applicationEvent) {
-                u.p("Apple API: open file called:" + applicationEvent);
-                u.p("filename = " + applicationEvent.getFilename());
-                String filename = applicationEvent.getFilename();
-                try {
-                    main.getLoadDeskletAction().load(new File(filename).toURL());
-                } catch (MalformedURLException ex) {
-                    ex.printStackTrace();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                } catch (JDOMException ex) {
-                    ex.printStackTrace();
-                } catch (LifeCycleException ex) {
-                    ex.printStackTrace();
+        try {
+            u.p("installing mac support");
+            Application.getApplication().addApplicationListener(new ApplicationListener() {
+                public void handleAbout(ApplicationEvent applicationEvent) {
+                    u.p("Apple API: about called:" + applicationEvent);
                 }
-            }
-            public void handlePreferences(ApplicationEvent applicationEvent) {
-                u.p("Apple API: prefs called:" + applicationEvent);
-            }
-            public void handlePrintFile(ApplicationEvent applicationEvent) {
-                u.p("Apple API: print called:" + applicationEvent);
-            }
-            public void handleQuit(ApplicationEvent applicationEvent) {
-                u.p("Apple API: quit called:" + applicationEvent);
-                main.getQuitAction().actionPerformed(
-                        new ActionEvent(this,-1,"exit"));
-            }
-            public void handleReOpenApplication(ApplicationEvent applicationEvent) {
-                u.p("Apple API: re-open called:" + applicationEvent);
-            }
-        });
-        u.p("mac support installed");
+                public void handleOpenApplication(ApplicationEvent applicationEvent) {
+                    u.p("Apple API: open app called:" + applicationEvent);
+                }
+                public void handleOpenFile(ApplicationEvent applicationEvent) {
+                    u.p("Apple API: open file called:" + applicationEvent);
+                    u.p("filename = " + applicationEvent.getFilename());
+                    String filename = applicationEvent.getFilename();
+                    try {
+                        main.getLoadDeskletAction().load(new File(filename).toURL());
+                    } catch (MalformedURLException ex) {
+                        ex.printStackTrace();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    } catch (JDOMException ex) {
+                        ex.printStackTrace();
+                    } catch (LifeCycleException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                public void handlePreferences(ApplicationEvent applicationEvent) {
+                    u.p("Apple API: prefs called:" + applicationEvent);
+                }
+                public void handlePrintFile(ApplicationEvent applicationEvent) {
+                    u.p("Apple API: print called:" + applicationEvent);
+                }
+                public void handleQuit(ApplicationEvent applicationEvent) {
+                    u.p("Apple API: quit called:" + applicationEvent);
+                    main.getQuitAction().actionPerformed(
+                            new ActionEvent(this,-1,"exit"));
+                }
+                public void handleReOpenApplication(ApplicationEvent applicationEvent) {
+                    u.p("Apple API: re-open called:" + applicationEvent);
+                }
+            });
+            u.p("mac support installed");
+        }catch (Throwable thr) {
+            u.p("installing mac support failed");
+            u.p(thr.getMessage());
+            thr.printStackTrace();
+        }
     }
     
 }
