346e01ad13d4d1757ea40e3907150588ace06d17
kebernet <kebernet@40bb2de6-3f2a-0410-b0ae-47b76eba00f0>
2007-03-12 16:58:21 +0000

    ""

---
+++ trunk/std_desklets/JRubyConsole/src/com/totsp/desklet/ruby/RubyDesklet.java
@@ -0,0 +1,51 @@
+/*
+ * RubyDesklet.java
+ *
+ * Created on March 12, 2007, 9:02 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.totsp.desklet.ruby;
+
+import ab5k.desklet.Desklet;
+import ab5k.desklet.DeskletContainer;
+import ab5k.desklet.DeskletContext;
+import java.awt.Dimension;
+
+/**
+ *
+ * @author cooper
+ */
+public class RubyDesklet implements Desklet{
+    
+    DeskletContext context;
+    /** Creates a new instance of RubyDesklet */
+    public RubyDesklet() {
+    }
+    
+    public void init(DeskletContext context) throws Exception {
+        context = context;
+        DeskletContainer container = context.getContainer();
+        final IRBConsole console = new IRBConsole();
+        container.setShaped( false );
+        container.setResizable( true );
+        container.setBackgroundDraggable( false );
+        container.setContent( console );
+        container.setSize( new Dimension( 250, 250 ));
+        console.start();
+        container.setVisible(true);
+    }
+    
+    public void stop() throws Exception {
+        context.notifyStopped();
+    }
+    
+    public void start() throws Exception {
+    }
+    
+    public void destroy() throws Exception {
+    }
+    
+}
