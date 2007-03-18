feeb4c8b0cfa1e11d974abd776a7d818c3c0fa27
joshyx <joshyx@40bb2de6-3f2a-0410-b0ae-47b76eba00f0>
2007-03-18 02:17:49 +0000

    implemented buffering for gradient background

--- container/src/java/ab5k/backgrounds/GradientBackground.java
+++ container/src/java/ab5k/backgrounds/GradientBackground.java
@@ -18,6 +18,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import org.jdesktop.swingx.painter.PinstripePainter;
 
 /**
@@ -25,27 +26,61 @@ import org.jdesktop.swingx.painter.PinstripePainter;
  * @author joshy
  */
 public class GradientBackground extends DesktopBackground {
+    private BufferedImage background;
+    
+    private static final boolean useBuffering = true;
+    
     
     /** Creates a new instance of GradientBackground */
     public GradientBackground() {
         setBackgroundName("Blue Gradient");
     }
-
-    public void paint(Graphics2D g) {
+    
+    public void paint(Graphics2D gc) {
         if (getDesktopPane() == null) {
             return;
         }
         
-        g = (Graphics2D) g.create();
+        
+        if(useBuffering) {
+            if (background == null || background.getWidth() !=
+                    desktopPane.getWidth() ||
+                    background.getHeight() != desktopPane.getHeight()) {
+                background = desktopPane.getGraphicsConfiguration().createCompatibleImage(
+                        desktopPane.getWidth(),
+                        desktopPane.getHeight());
+                Graphics2D g = background.createGraphics();
+                drawBG(g);
+                g.dispose();
+            }
+            gc.drawImage(background, 0, 0, desktopPane);
+        } else {
+            Graphics2D g = (Graphics2D) gc.create();
+            drawBG(g);
+            g.dispose();
+        }
+        
+    }
+    
+    private void drawBG(final Graphics2D g) {
         // the background
-        g.setPaint(new GradientPaint(new Point(0,0), 
+        g.setPaint(new GradientPaint(new Point(0,0),
                 new Color(190,190,204),
                 new Point(desktopPane.getWidth(), desktopPane.getHeight()),
                 new Color(50,50,204)));
         g.fillRect(0, 0, desktopPane.getWidth(), desktopPane.getHeight());
         
+        
+        // draw the pinstripe
         PinstripePainter pin = new PinstripePainter(new Color(50,50,204,30), 45.0,8.0,8.0);
         Graphics2D gfx = (Graphics2D) g.create();
+        
+        //set the clip
+        if (gfx.getClip() == null) {
+            gfx.setClip(0, 0, desktopPane.getWidth(),
+                    desktopPane.getHeight());
+        }
+        
         pin.paint(gfx, desktopPane, desktopPane.getWidth(), desktopPane.getHeight());
         gfx.dispose();
         
@@ -59,7 +94,6 @@ public class GradientBackground extends DesktopBackground {
         g.translate(-(bounds.getX()+bounds.getWidth())/2,
                 0-bounds.getY()-bounds.getHeight()/2);
         g.drawString(text,0,0);
-        g.dispose();
     }
     
 }
