7a2ac19176f01bab4c188f9789e93f241c3fae8f
joshyx <joshyx@40bb2de6-3f2a-0410-b0ae-47b76eba00f0>
2007-03-27 22:59:50 +0000

    we need to modify the spec to properly support dialogs and handle backwards compatibility

---
+++ branches/spec_mod_01/src/ab5k/desklet/test/TestDeskletContext.java
@@ -0,0 +1,108 @@
+package ab5k.desklet.test;
+
+import ab5k.desklet.DeskletContainer;
+import ab5k.desklet.DeskletContext;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.geom.Dimension2D;
+import java.io.File;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+class TestDeskletContext implements DeskletContext {
+    
+    private DeskletContainer frame;
+    private DeskletContainer dockingFrame;
+    private Map<String, String> prefs;
+    
+    public TestDeskletContext(JFrame frame, JFrame dockingFrame) {
+        super();
+        prefs = new HashMap<String,String>();
+        this.frame = new TestContainer(frame);
+        this.dockingFrame = new TestContainer( dockingFrame );
+    }
+    
+    public void closeRequest() {
+    }
+    
+    public Container getConfigurationContainer() {
+        return null;
+    }
+    
+    public DeskletContainer getContainer() {
+        return frame;
+    }
+    
+    public Container getDialog() {
+        return null;
+    }
+    
+    public String getPreference(String name, String defaultValue) {
+        if(prefs.containsKey(name)) {
+            return prefs.get(name);
+        }
+        return defaultValue;
+    }
+    
+    public void notifyStopped() {
+    }
+    
+    public String setPreference(String name, String value) {
+        prefs.put(name,value);
+        return value;
+    }
+    
+    public void showURL(URI uri) {
+    }
+    
+    
+    public DeskletContainer getDockingContainer() {
+        return dockingFrame;
+    }
+    
+    public void setShutdownWhenIdle(boolean shutdownWhenIdle) {
+    }
+    
+    public File getWorkingDirectory() {
+        // TODO
+        return null;
+    }    
+    
+    private static class TestContainer implements DeskletContainer {
+        
+        JFrame frame;
+        
+        TestContainer( JFrame frame ){
+            this.frame = frame;
+        }
+        
+        public void setContent(JComponent component) {
+            frame.add( component );
+        }
+        
+        public void setSize(Dimension2D size) {
+            frame.setSize(new Dimension((int)size.getWidth(),(int)size.getHeight()));
+        }
+        
+        public void setVisible(boolean visible) {
+        }
+        
+        public void setShaped(boolean shaped) {
+        }
+        
+        public void setResizable(boolean resizable) {
+            frame.setResizable(false);
+        }
+        
+        public void setBackgroundDraggable(boolean backgroundDraggable) {
+        }
+        
+        public Dimension2D getSize() {
+            return frame.size();
+        }
+        
+    }
+}
