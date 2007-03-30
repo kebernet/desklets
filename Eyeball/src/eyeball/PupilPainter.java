e43b01e23c7e94f8ebe4594a72915e6935e87b65
joshyx <joshyx@40bb2de6-3f2a-0410-b0ae-47b76eba00f0>
2007-03-30 22:35:19 +0000

    updated to use the GlobalMouse service

--- Eyeball/src/eyeball/PupilPainter.java
+++ Eyeball/src/eyeball/PupilPainter.java
@@ -11,13 +11,20 @@ import org.jdesktop.swingx.painter.AbstractPainter;
 
 
 class PupilPainter extends AbstractPainter<JPanel> {
+    private Point mouse;
+    public PupilPainter() {
+        mouse = new Point(0,0);
+    }
+    public void setMouseLocation(Point pt) {
+        this.mouse = pt;
+    }
 
     protected void doPaint(Graphics2D gfx, JPanel component, int width, int height) {
         Graphics2D g = (Graphics2D) gfx.create();
         g.setPaint(Color.WHITE);
-        Point pt = MouseInfo.getPointerInfo().getLocation();
+        Point pt = mouse;//MouseInfo.getPointerInfo().getLocation();
         Point pt2 = new Point(width / 2, height / 2);
-        SwingUtilities.convertPointToScreen(pt2, component);
+        //SwingUtilities.convertPointToScreen(pt2, component);
         double dist = pt.distance(pt2);
         double ang = Main.calcAngle(pt, pt2);
         g = (Graphics2D) g.create();
