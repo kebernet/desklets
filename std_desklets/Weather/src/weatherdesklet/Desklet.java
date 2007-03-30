210b9ea2d07dd4574d9c024497566dffd2c57f58
joshyx <joshyx@40bb2de6-3f2a-0410-b0ae-47b76eba00f0>
2007-03-30 23:14:30 +0000

    removed dead code

--- std_desklets/Weather/src/weatherdesklet/Desklet.java
+++ std_desklets/Weather/src/weatherdesklet/Desklet.java
@@ -30,9 +30,19 @@ public class Desklet extends AbstractDesklet{
         super();
     }
     
-    public void destroy() throws Exception {
-    }
     
+    public void init(DeskletContext context) throws Exception {
+        this.context = context;
+        dockLabel = new JLabel();
+        context.getDockingContainer().setContent(dockLabel);
+        display = new WeatherWatcher(this);
+        context.getContainer().setContent(display);
+        context.getContainer().setBackgroundDraggable(true);
+        context.getContainer().setResizable(false);
+        context.getContainer().setShaped(true);
+        context.getContainer().setVisible(true);
+        stationID = context.getPreference("STATION_ID","KATL");
+    }
     
     Thread thread;
     public void start() throws Exception {
@@ -42,14 +52,13 @@ public class Desklet extends AbstractDesklet{
                 while( running ){
                     // check the weather
                     updateWeather();
-                    // sleep for 30 min
+                    // sleep for 5 min
                     try {
                         Thread.currentThread().sleep(5 * 60 * 1000); //sleep for 5 min
                     } catch (InterruptedException ex) {
-                        ex.printStackTrace();
+                        //ex.printStackTrace();
                     }
                 }
-                u.p("stopping");
                 context.notifyStopped();
             }
             
@@ -64,16 +73,7 @@ public class Desklet extends AbstractDesklet{
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        u.p("got weather: " + wth);
         display.setWeather(wth);
-        /*
-        if(wth == null) {
-            dockLabel.setText("-99 unknown");
-            dockLabel.setIcon(display.icons16.get(Weather.UNKNOWN));
-        } else {
-            dockLabel.setText(wth.getTempF()+" " + wth.getWeather());
-            dockLabel.setIcon(display.icons16.get(wth.getType()));
-        }*/
     }
     
     public void stop() throws Exception {
@@ -81,24 +81,14 @@ public class Desklet extends AbstractDesklet{
         thread.interrupt();
     }
     
+    public void destroy() throws Exception {
+    }
+    
+    
     public DeskletContext getContext() {
         return this.context;
     }
     
-    public void init(DeskletContext context) throws Exception {
-        this.context = context;
-        dockLabel = new JLabel();
-        context.getDockingContainer().setContent(dockLabel);
-        display = new WeatherWatcher(this);
-        context.getContainer().setContent(display);
-        context.getContainer().setBackgroundDraggable(true);
-        context.getContainer().setResizable(false);
-        context.getContainer().setShaped(true);
-        context.getContainer().setVisible(true);
-        
-        stationID = context.getPreference("STATION_ID","KATL");
-        u.p("station ID = " + stationID);
-    }
     
     public String getStationID() {
         return stationID;
