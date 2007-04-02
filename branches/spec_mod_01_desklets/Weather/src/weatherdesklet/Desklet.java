71e491192a64eb62d5d254e0f793795dc2341846
joshyx <joshyx@40bb2de6-3f2a-0410-b0ae-47b76eba00f0>
2007-04-02 22:08:27 +0000

    updated to new api?

---
+++ branches/spec_mod_01_desklets/Weather/src/weatherdesklet/Desklet.java
@@ -0,0 +1,105 @@
+/*
+ * Desklet.java
+ *
+ * Created on August 4, 2006, 8:48 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package weatherdesklet;
+
+import ab5k.desklet.AbstractDesklet;
+import ab5k.desklet.DeskletContext;
+import javax.swing.JLabel;
+import org.joshy.util.u;
+import org.joshy.weather.Weather;
+import org.joshy.weather.WeatherFactory;
+/**
+ *
+ * @author cooper
+ */
+public class Desklet extends AbstractDesklet{
+    
+    private WeatherWatcher display;
+    JLabel dockLabel;
+    private boolean running = false;;
+    private String stationID = null;
+    /** Creates a new instance of Desklet */
+    public Desklet() {
+        super();
+    }
+    
+    
+    public void init(DeskletContext context) throws Exception {
+        this.context = context;
+        dockLabel = new JLabel();
+        context.getDockingContainer().setContent(dockLabel);
+        display = new WeatherWatcher(this);
+        context.getContainer().setContent(display);
+        context.getContainer().setBackgroundDraggable(true);
+        context.getContainer().setResizable(false);
+        context.getContainer().setShaped(true);
+        context.getContainer().setVisible(true);
+        stationID = context.getPreference("STATION_ID","KATL");
+    }
+    
+    Thread thread;
+    public void start() throws Exception {
+        thread = new Thread(new Runnable() {
+            public void run() {
+                running = true;
+                while( running ){
+                    // check the weather
+                    updateWeather();
+                    // sleep for 5 min
+                    try {
+                        Thread.currentThread().sleep(5 * 60 * 1000); //sleep for 5 min
+                    } catch (InterruptedException ex) {
+                        //ex.printStackTrace();
+                    }
+                }
+                context.notifyStopped();
+            }
+            
+        });
+        thread.start();
+    }
+    
+    private void updateWeather() {
+        Weather wth = null;
+        try {
+            wth = WeatherFactory.newInstance().getWeather(getStationID());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        display.setWeather(wth);
+    }
+    
+    public void stop() throws Exception {
+        this.running = false;
+        thread.interrupt();
+    }
+    
+    public void destroy() throws Exception {
+    }
+    
+    
+    public DeskletContext getContext() {
+        return this.context;
+    }
+    
+    
+    public String getStationID() {
+        return stationID;
+    }
+    
+    public void setStationID(String stationID) {
+        this.stationID = stationID;
+        System.out.println("saved station ID"+ stationID);
+        context.setPreference("STATION_ID",stationID);
+    }
+    
+    
+    
+}
