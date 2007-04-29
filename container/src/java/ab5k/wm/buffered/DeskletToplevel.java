8d2030509d5711529d1522e097c7a4e5f82ca9f0
joshyx <joshyx@40bb2de6-3f2a-0410-b0ae-47b76eba00f0>
2007-04-29 05:48:40 +0000

    debugging tools removed some dead code

--- container/src/java/ab5k/wm/buffered/DeskletToplevel.java
+++ container/src/java/ab5k/wm/buffered/DeskletToplevel.java
@@ -1,8 +1,16 @@
 package ab5k.wm.buffered;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Graphics;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import org.joshy.util.u;
 
 
 public class DeskletToplevel extends JPanel {
@@ -11,6 +19,7 @@ public class DeskletToplevel extends JPanel {
     DeskletToplevel(BufferedDeskletContainer container) {
         setOpaque(false);
         this.container = container;
+        this.setDoubleBuffered(false);
     }
     protected void paintComponent(Graphics g) {
         //g.setColor(Color.GREEN);
@@ -18,6 +27,20 @@ public class DeskletToplevel extends JPanel {
         super.paintComponent(g);
         //g.setColor(Color.RED);
         //g.drawString("desklet top level",5,15);
+    }
+    private void printFlags(Component c) {
+        try {
+            java.lang.reflect.Method m = JComponent.class.getDeclaredMethod("getFlag", java.lang.Integer.TYPE);
+            m.setAccessible(true);
+            u.p("ancestor using buffer = " + m.invoke(c, new java.lang.Integer(1)));
+            u.p("is painting tile = " + m.invoke(c, new java.lang.Integer(2)));
+        } catch (Throwable ex) {
+            u.p(ex);
+            Logger.getLogger("global").log(Level.SEVERE, null, ex);
+        }
+    }
+    public boolean isOptimizedDrawingEnabled() {
+        return false;
     }
 
     BufferedDeskletContainer getContainer() {
