a1a87aa90b0540f7a89707d99af56ed18509a07c
joshyx <joshyx@40bb2de6-3f2a-0410-b0ae-47b76eba00f0>
2007-05-10 20:25:10 +0000

    force AA to be on

--- trunk/container/src/main/java/org/glossitope/container/wm/buffered/DeskletToplevel.java
+++ trunk/container/src/main/java/org/glossitope/container/wm/buffered/DeskletToplevel.java
@@ -1,6 +1,8 @@
 package org.glossitope.container.wm.buffered;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import javax.swing.JPanel;
 
@@ -14,11 +16,27 @@ public class DeskletToplevel extends JPanel {
         //this.setDoubleBuffered(false);
     }
     protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
         //g.setColor(Color.GREEN);
         //g.fillRect(0, 0, getWidth(), getHeight());
-        super.paintComponent(g);
+        super.paintComponent(g2);
+        g2.dispose();
         //g.setColor(Color.RED);
         //g.drawString("desklet top level",5,15);
+    }
+    
+    protected void paintChildren(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        super.paintChildren(g2);
+        g2.dispose();
     }
 
     BufferedDeskletContainer getContainer() {
