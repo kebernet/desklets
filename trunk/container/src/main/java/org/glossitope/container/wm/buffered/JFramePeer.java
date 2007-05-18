fb00d13313837753c910129c616b9f11abda9a25
joshyx <joshyx@40bb2de6-3f2a-0410-b0ae-47b76eba00f0>
2007-05-18 01:20:33 +0000

    fixes for shaped windows on mac

--- trunk/container/src/main/java/org/glossitope/container/wm/buffered/JFramePeer.java
+++ trunk/container/src/main/java/org/glossitope/container/wm/buffered/JFramePeer.java
@@ -11,18 +11,22 @@ package org.glossitope.container.wm.buffered;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Shape;
 import java.awt.Window;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
 
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.RootPaneContainer;
 import javax.swing.SwingUtilities;
 
@@ -31,6 +35,9 @@ import org.joshy.util.u;
 import org.glossitope.container.util.GraphicsUtil;
 import org.glossitope.container.util.MoveMouseListener;
 import org.glossitope.container.util.PlafUtil;
+import org.jdesktop.swingx.JXPanel;
+import org.jdesktop.swingx.painter.CheckerboardPainter;
+import org.jdesktop.swingx.painter.Painter;
 import org.jdesktop.swingx.util.WindowUtils;
 
 /**
@@ -48,6 +55,7 @@ public class JFramePeer extends DCPeer {
     private boolean shaped = false;
     private boolean isDialog = false;
     private JComponent content;
+    private JXPanel top;
     
     /** Creates a new instance of JFrameDeskletContainer */
     public JFramePeer(final BufferedDeskletContainer bdc) {
@@ -55,6 +63,17 @@ public class JFramePeer extends DCPeer {
     }
     public JFramePeer(final BufferedDeskletContainer bdc, boolean isDialog) {
         super(bdc);
+        this.top = new JXPanel();
+        this.top.setBackgroundPainter(new Painter() {
+            
+            public void paint(Graphics2D arg0, Object arg1, int arg2, int arg3) {
+                arg0.setColor(Color.BLACK);
+                arg0.fillRect(0,0,30,30);
+            }
+            //new CheckerboardPainter(Color.BLACK, Color.BLUE)
+        });//new Color(0,0,0,0)));
+        this.top.setOpaque(false);
+        
         this.isDialog = isDialog;
         if(isDialog) {
             JFrame parent = ((JFramePeer)bdc.getProxy().contentContainer.getPeer()).frame;
@@ -69,10 +88,10 @@ public class JFramePeer extends DCPeer {
                 public void componentHidden(ComponentEvent e) {
                 }
                 public void componentMoved(ComponentEvent e) {
-                    u.p("frame moved! " + e);
+                    //u.p("frame moved! " + e);
                     if(frame.getLocation().getX() >
                             bdc.wm.core.getCollapseWindowAction().getClosedBounds().getX()) {
-                        u.p("once more into the dock!");
+                        //u.p("once more into the dock!");
                         SwingUtilities.invokeLater(new Runnable() {
                             public void run() {
                                 //bdc.wm.convertExternalToInternalContainer(JFramePeer.this);
@@ -86,36 +105,83 @@ public class JFramePeer extends DCPeer {
                 }
             });
         }
+        if(PlafUtil.isMacOSX()) {
+            JPanel contentPane = new JPanel();
+            contentPane.setBackground(Color.BLUE);
+            contentPane.setOpaque(false);
+            this.root.setContentPane(contentPane);
+        }
+        this.root.getContentPane().add(top);
     }
     
     public void setContent(JComponent content) {
         if(this.content != null) {
-            root.getContentPane().remove(content);
+            //root.getContentPane().remove(content);
         }
         this.content = content;
         if(this.content != null) {
-            root.getContentPane().add(content);
+            this.top.add(content);
+            //root.getContentPane().add(content);
         }
     }
     
+    private boolean backgroundDraggable = false;
     public void setBackgroundDraggable(boolean b) {
+        this.backgroundDraggable = b;
+        /*
         u.p("set background draggable to: " + b);
-        this.content.addMouseMotionListener(new MoveMouseListener(bdc,bdc.wm) {
-            Point2D getScreenLocation() {
-                return frame.getLocation();
-            }
-            Point getScreenLocation(MouseEvent e) {
-                Point cursor = e.getPoint();
-                Point2D target_location = frame.getLocation();
-                return new Point(
-                        (int)(target_location.getX()+cursor.getX()),
-                        (int)(target_location.getY()+cursor.getY()));
-            }
-            
-        });
+        JFrameMoveMouseListener ml = new JFrameMoveMouseListener();
+        this.content.addMouseListener(ml);
+        this.content.addMouseMotionListener(ml);*/
     }
+    
+    private class JFrameMoveMouseListener implements MouseListener, MouseMotionListener {
+        Point start_drag;
+        Point start_loc;
+        
+        public void mouseClicked(MouseEvent arg0) {
+        }
+        
+        public void mousePressed(MouseEvent arg0) {
+            u.p("mouse pressed: " + arg0);
+            start_drag = arg0.getPoint();
+            u.p("start drag = " + start_drag);
+            SwingUtilities.convertPointToScreen(start_drag, frame);
+            u.p("start drag = " + start_drag);
+            //this.start_loc = this.getFrame(this.target).getLocation();
+            start_loc = GraphicsUtil.toPoint(frame.getLocation());
+            u.p("start loc = " + start_loc);
+        }
+        
+        public void mouseReleased(MouseEvent arg0) {
+        }
+        
+        public void mouseEntered(MouseEvent arg0) {
+        }
+        
+        public void mouseExited(MouseEvent arg0) {
+        }
+        
+        public void mouseDragged(MouseEvent e) {
+            u.p("mouse dragged: " + e);
+            Point current = e.getPoint();
+            SwingUtilities.convertPointToScreen(current, frame);
+            Point offset = new Point(
+                    (int)current.getX()-(int)start_drag.getX(),
+                    (int)current.getY()-(int)start_drag.getY());
+            Point new_location = new Point(
+                    (int)(this.start_loc.getX()+offset.getX()),
+                    (int)(this.start_loc.getY()+offset.getY()));
+            frame.setLocation(new_location);
+        }
+        
+        public void mouseMoved(MouseEvent arg0) {
+        }
+        
+    }
+    
     public boolean isBackgroundDraggable() {
-        return false;
+        return this.backgroundDraggable;
     }
     
     
@@ -126,6 +192,7 @@ public class JFramePeer extends DCPeer {
                 if(PlafUtil.isMacOSX()) {
                     frame.setUndecorated(true);
                     frame.setBackground(new Color(0,0,0,0));
+                    //com.sun.jna.examples.WindowUtils.setWindowTransparent(frame, true);
                 } else {
                     com.sun.jna.examples.WindowUtils.setWindowTransparent(frame, true);
                 }
